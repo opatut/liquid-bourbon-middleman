6673fdac5a15d1be13c3808deea637cdc4679864
Paul Bienkowski <paul.bienkowski@sinnerschrader.com>
2015-01-27 14:25:42 +0100

    Navigation

--- source/app/scripts/_main-navi.js
+++ source/app/scripts/_main-navi.js
@@ -9,3 +9,17 @@ $('.js-menu-screen').on('click touchstart', function(e){
   $('.js-menu-screen').toggleClass('is-visible');
   e.preventDefault();
 });
+
+$(document).ready(function() {
+  var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
+  $('#js-centered-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-centered-navigation-menu').slideToggle(function(){
+      if($('#js-centered-navigation-menu').is(':hidden')) {
+        $('#js-centered-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
